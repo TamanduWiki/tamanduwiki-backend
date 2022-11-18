8b038d03803f4d55d5641f0dea9d556e71306d75
abacchi00 <andre.rossini.bacchi@gmail.com>
2022-11-17 21:36:49 -0300

    fix: cors problems

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 import { mainRoutes } from '@/routes';
 
 const app = express();
+
 const PORT = process.env.PORT || 3333;
+const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN;
+
+app.use(cors({ origin: FRONTEND_ORIGIN }));
 
 app.use(express.json());
 
