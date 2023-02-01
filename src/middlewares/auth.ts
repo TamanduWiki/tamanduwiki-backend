abd383c4f3da6b0a270b1e0a3f8d1ce052fd9ac1
abacchi00 <andre.rossini.bacchi@gmail.com>
2023-01-31 22:24:49 -0300

    feat: auth features

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,34 +1,48 @@
-import { User } from "@/entities/User";
 import { NextFunction, Request, Response } from "express";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import jwt, { JsonWebTokenError, JwtPayload } from "jsonwebtoken";
 
-export const authenticateUser = (request: Request, response: Response, next: NextFunction) => {
+import { User } from "@/entities/User";
+
+export const authenticateUser = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
   try {
-    const token = request.headers['authorization'];
+    const token = request.headers["authorization"];
 
     if (!token) {
       return response.status(401).json({
         auth: false,
-        message: 'Nenhum token providenciado.',
+        error: "auth-no-token-error",
+        message: "Nenhum token providenciado.",
       });
     }
 
-    jwt.verify(token as string, process.env.JWT_SECRET as string, (err, decoded) => {
-      if (err) {
-        return response.status(500).json({
-          auth: false,
-          message: err?.message || 'Houve uma falha no sistema de autenticação.',
-        });
+    jwt.verify(
+      token as string,
+      process.env.JWT_SECRET as string,
+      (err, decoded) => {
+        if (err) throw err;
+
+        request.user = decoded as Omit<User, "password"> & JwtPayload;
+
+        next();
       }
+    );
+  } catch (err: any) {
+    if (err instanceof JsonWebTokenError) {
+      return response.status(500).json({
+        auth: false,
+        error: "auth-invalid-or-expired-token-error",
+        message: err?.message || "Houve uma falha no sistema de autenticação.",
+      });
+    }
 
-      request.user = decoded as Omit<User, 'password'> & JwtPayload;
-
-      next();
-    });
-  } catch(err) {
     return response.status(500).json({
       auth: false,
-      message: 'Houve uma falha no sistema de autenticação.',
+      error: "auth-unexpected-error",
+      message: err?.message || "Houve uma falha no sistema de autenticação.",
     });
   }
-}
+};
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -9,20 +9,26 @@ import { getUserController } from "@/useCases/GetUser";
 
 const router = Router();
 
-router.post('/users', (request, response) => {
+router.post("/users", (request, response) => {
   return createUserController.handle(request, response);
 });
 
-router.get('/users', authenticateUser, (request, response) => {
+router.get("/users", authenticateUser, (request, response) => {
   return listUsersController.handle(request, response);
-})
+});
 
-router.post('/login', (request, response) => {
-  return loginController.handle(request, response);
-})
-
-router.get('/user-info', authenticateUser, (request, response) => {
+router.get("/user-info", authenticateUser, (request, response) => {
   return getUserController.handle(request, response);
-})
+});
+
+// Auth
+
+router.post("/login", (request, response) => {
+  return loginController.handle(request, response);
+});
+
+router.get("/ensure-authenticated", authenticateUser, (request, response) => {
+  return response.status(200).json({ auth: true });
+});
 
 export { router };
